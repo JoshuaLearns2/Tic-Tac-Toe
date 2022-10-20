3642a1bc89562d1222b19c7aa44cbbf58595b035
Your Name <jlmeeks587@gmail.com>
2022-10-20 00:20:09 -0400

    Got the styling sort of where I want it and now just working out the logic in JS

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
-console.log('hello')
+const X_MARKER = 'X-marker';
+const O_MARKER = 'O-marker';
+
+let yourTurn;
+
+const place = document.querySelectorAll('[data-board]')
+
+place.forEach(board => {
+  board.addEventListener('click', handleClick, { once: true })
+})
+
+function handleClick(e) {
+  const board = e.target;
+
+  const currentTurn = yourTurn ? X_MARKER : O_MARKER;
+
+  const X = document.createElement('div');
+  const O = document.createElement('div');
+
+  const X_turn = place.appendChild(X);
+  const O_turn = place.appendChild(0);
+
+  X.classList.add('X-marker');
+  O.classList.add('O-marker');
+
+  X.textContent = 'X';
+  O.textContent = 'O';
+
+}
+
+function placeMarker(board, currentTurn)
+
+
